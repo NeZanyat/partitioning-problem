557125212ffaf02b1ea6e5b80bb2fda2e92be9f9
Roman <roman@Pro-15.local>
2019-10-03 14:35:19 +0300

    Clean up

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import bisect
 # O(n * (log(n) + n))
 def is_partitionable(numbers: t.List[int]) -> bool:
 
-    if numbers is None:
-        return False
-
     if any(n < 0 for n in numbers):
         raise Exception("Invalid numbers - should be a positive integers")
+
+    if numbers is None:
+        return False
 
     if len(numbers) < 2:
         return False
